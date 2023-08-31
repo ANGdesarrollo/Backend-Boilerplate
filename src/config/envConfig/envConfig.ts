671a51b9a005366ad152aa222feb669e00147691
ANGDesarrollo <alexisgraff123@gmail.com>
2023-08-30 23:18:52 -0300

    feat/ag/ added mapping to routes and interface for the iteration

--- src/config/envConfig/envConfig.ts
+++ src/config/envConfig/envConfig.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import { IEnvConfig } from './IEnvConfig';
+import * as process from 'process';
 dotenv.config();
 
 if (!process.env.NODE_ENV)
@@ -12,7 +13,19 @@ if (!process.env.NODE_PORT)
     throw new Error('Environment not properly set. PORT not set');
 }
 
+if (!process.env.NODE_URL_API)
+{
+    throw new Error('Environment not properly set. URL_API not set');
+}
+
+if (!process.env.NODE_URL_WEB)
+{
+    throw new Error('Environment not properly set. URL_WEB not set');
+}
+
 export const env: IEnvConfig = {
     NODE_ENV: process.env.NODE_ENV,
-    NODE_PORT: Number(process.env.NODE_PORT)
+    NODE_PORT: Number(process.env.NODE_PORT),
+    NODE_URL_API: process.env.NODE_URL_API,
+    NODE_URL_WEB: process.env.NODE_URL_WEB
 };
---
+++ src/config/loggerConfig/pinoLogger.ts
@@ -0,0 +1,3 @@
+export const pinoLogger = {
+    prettyPrint: true
+};
